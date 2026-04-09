5e7c3570dd2c273672514ae62a76bf80bcfa097a
Josephquito <jdquito.3@sudamericano.edu.ec>
2026-04-09 10:34:24 -0500

    fix redis

--- src/campaigns/campaigns.module.ts
+++ src/campaigns/campaigns.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CampaignsController } from './campaigns.controller';
-import { CampaignsService } from './campaigns.service';
-import { CampaignProcessor } from './campaign.processor';
 import { CampaignTrackerService } from './campaign-tracker.service';
 import { ChatwootModule } from '../chatwoot/chatwoot.module';
 import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [ChatwootModule, ConfigModule],
-  controllers: [CampaignsController],
-  providers: [CampaignsService, CampaignProcessor, CampaignTrackerService],
-  exports: [CampaignsService, CampaignTrackerService],
+  providers: [CampaignTrackerService],
+  exports: [CampaignTrackerService],
 })
 export class CampaignsModule {}
